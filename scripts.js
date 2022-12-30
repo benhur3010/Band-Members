015a6396e5d8a61dc00d80547c6436085d85a83a
Ben-Hur <benhurbueno.adv@gmail.com>
2022-12-30 11:03:46 -0300

    finished

--- scripts.js
+++ scripts.js
@@ -1,24 +1,37 @@
 members = [
-    {
-        fname: 'John',
-        lname: 'Lennon',
-        instrument: 'Violão'
-    },
-    {
-        fname: 'George',
-        lname: 'Harrison',
-        instrument: 'Guitarra'
-    }
-]
+  {
+    fname: "John",
+    lname: "Lennon",
+    instrument: "Violão"
+  },
+  {
+    fname: "George",
+    lname: "Harrison",
+    instrument: "Guitarra"
+  }
+];
 
 const handlingForms = {
-
-    data() {
-        return {
-            members: window.members
-        }
-    },
-
+  data() {
+    return {
+      members: window.members,
+      newMember: {}
+    };
+  },
+  methods: {
+    addMember: function () {
+      if (
+        this.newMember.fname &&
+        this.newMember.lname &&
+        this.newMember.instrument
+      ) {
+        this.members.push(this.newMember);
+        this.newMember = {};
+      } else {
+        alert("All fields must be filled");
+      }
+    }
+  }
 };
 
-Vue.createApp(handlingForms).mount('#app');
+Vue.createApp(handlingForms).mount("#app");
